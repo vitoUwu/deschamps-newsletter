ad3df78736ad2c62ef51903177a1707e5aa68fc1
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-12-19 22:46:22 -0400

    feat: add logs exporter

--- src/observability/index.ts
+++ src/observability/index.ts
@@ -1,5 +1,12 @@
+import { logs } from "@opentelemetry/api-logs";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 import { resourceFromAttributes } from "@opentelemetry/resources";
+import {
+  ConsoleLogRecordExporter,
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+} from "@opentelemetry/sdk-logs";
 import {
   ConsoleMetricExporter,
   PeriodicExportingMetricReader,
@@ -11,11 +18,27 @@ import {
   ATTR_SERVICE_VERSION,
 } from "@opentelemetry/semantic-conventions";
 
+const resource = resourceFromAttributes({
+  [ATTR_SERVICE_NAME]: "newsletter",
+  [ATTR_SERVICE_VERSION]: "0.0.1",
+});
+
+const logsExporter =
+  process.env.OTEL_LOGS_EXPORTER === "otlp"
+    ? new OTLPLogExporter({
+        url: `${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/logs`,
+      })
+    : new ConsoleLogRecordExporter();
+
+const loggerProvider = new LoggerProvider({
+  resource,
+  processors: [new SimpleLogRecordProcessor(logsExporter)],
+});
+
+logs.setGlobalLoggerProvider(loggerProvider);
+
 const sdk = new NodeSDK({
-  resource: resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: "newsletter",
-    [ATTR_SERVICE_VERSION]: "0.0.1",
-  }),
+  resource,
   traceExporter: new ConsoleSpanExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
